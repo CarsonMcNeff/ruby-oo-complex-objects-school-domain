234579cc2c38ef4157b3625ba0a5e3ff07830bf0
CarsonMcNeff <cjmcneff@outlook.com>
2020-12-15 12:31:56 -0600

    commit tree a42435bbd6058467ed79fbab1560164c503379e6

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student,grade)
+        roster[grade] = [] if !roster.key?(grade)
+        roster[grade] << student
+    end
+
+    def grade(grade)
+        roster.values_at(grade).flatten
+    end
+
+    def sort
+        roster.each do |grade, students|
+            roster[grade] = students.sort
+        end
+    end
+end
